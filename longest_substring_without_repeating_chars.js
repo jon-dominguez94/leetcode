1dc53106042212c04a74f2ad70ce1e96d4ea53d7
jondominguez94 <jon.dominguez94@yahoo.com>
2019-01-28 10:41:34 -0800

    Solve longest substring without repeating characters

---
+++ longest_substring_without_repeating_chars.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+
+function updateSub(set, char){
+    
+}
+
+var lengthOfLongestSubstring = function(s) {
+    let longest = "";
+    let chars = new Set();
+    
+    for(let i = 0; i < s.length; i++){
+        if(chars.has(s[i])){
+            const newWord = [...chars].join('');
+            const idx = newWord.indexOf(s[i]);
+            longest = newWord.length > longest.length ? newWord : longest;
+            chars = new Set(newWord.slice(idx + 1));
+        }
+        chars.add(s[i]);
+        
+    }
+    const lastWord = [...chars].join('');
+    longest = lastWord.length > longest.length ? lastWord : longest;
+    return longest.length;
+};
