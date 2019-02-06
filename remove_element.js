84272eedb3e3537eac605fd85c0f7cf0ea0c4078
jondominguez94 <jon.dominguez94@yahoo.com>
2019-02-05 17:06:24 -0800

    Refactor remove element

--- remove_element.js
+++ remove_element.js
@@ -3,20 +3,13 @@
  * @param {number} val
  * @return {number}
  */
-var removeElement = function(nums, val) {
-    let i = 0;
-    let count = 0;
-    for(let j = 0; j < nums.length; j++){
-        if(nums[i] !== val){
-            i = j;
-        } else {
-            if(nums[j] !== val){
-                [nums[i], nums[j]] = [nums[j], nums[i]]
-                i+=1;
-                count += 1;
-            }
-        }
+var removeElement = function (nums, val) {
+  let i = 0;
+  for (let j = 0; j < nums.length; j++) {
+    if (nums[j] !== val) {
+      nums[i] = nums[j];
+      i++;
     }
-    if(nums[i] === val) count++;
-    return count === 0 ? nums.length : i;
+  }
+  return i;
 };
